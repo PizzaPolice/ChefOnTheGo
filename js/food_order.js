86ed2b29869160cabed58d1c260562e2fd7e6c6d
audreyeckman <audreyeckman@yahoo.com>
2018-02-09 12:13:15 -0800

    foodOrder only first increment button working bc of getElementById

--- js/food_order.js
+++ js/food_order.js
@@ -1,64 +1,23 @@
-// Set counter default to zero
-//var value = 0
 
-// Display total
-$("#value").text(value);
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value<10){
+        value++;
+            document.getElementById('number').value = value;
+    }
+}
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value>1){
+        value--;
+            document.getElementById('number').value = value;
+    }
 
-// When button is clicked
-$("#plus-btn").click(function(){
-  //Add 1 to counter
-  value = value + 1;
-	// Display total
-	$("#value").text(value);
-});
-
-
-//Subtract
-$("#minus-btn").click(function(){
-  value = value - 1;
-  $("#value").text(value);
-});
-
-
+}
 
 
 
-
-
-/*
-$('.minus-btn').on('click', function(e) {
- e.preventDefault();
- var $this = $(this);
- var $input = $this.closest('div').find('input');
-
- var value = parseInt($input.val());
-
-        if (value > 1) {
-             value = value - 1;
-        } else {
-             value = 0;
-        }
-
-        $input.val(value);
-
-        });
-
-$('.plus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
-
-        if (value < 100) {
-            value = value + 1;
-         } else {
-            value =100;
-         }
-
-            $input.val(value);
-        });
-
-  $('.like-btn').on('click', function() {
-        $(this).toggleClass('is-active');
-   });
-*/
