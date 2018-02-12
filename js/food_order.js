2adf74af9b13e845730a272307a98e6f57d6601d
bluej-21 <jae916@gmail.com>
2018-02-12 13:07:55 -0800

    Made JS

--- js/food_order.js
+++ js/food_order.js
@@ -1,36 +1,33 @@
-function incrementValue()
+function incrementValue(id)
 {
-    var value = parseInt(document.getElementById('P').value, 10);
-
-
+    var value = parseInt(document.getElementById(id).value, 10);
     value = isNaN(value) ? 0 : value;
-    if(value<10){
+    if (value < 10) {
         value++;
-            document.getElementById('P').value = value;
+            document.getElementById(id).value = value;
     }
 
 
 
 }
-function decrementValue()
+function decrementValue(id)
 {
-    var value = parseInt(document.getElementById('P').value, 10);
+    var value = parseInt(document.getElementById(id).value, 10);
 
 
     value = isNaN(value) ? 0 : value;
     if(value>1){
         value--;
-            document.getElementById('P').value = value;
+            document.getElementById(id).value = value;
     }
 
 }
 
 
-/*
-function clearBox()
+
+function clearBox(id)
 {
-  document.getElementById('P').innerHTML = "";
-	}
+  document.getElementById(id).innerHTML = ""
+  listOfItems = document.getElementsByClassName("item")
 }
 
-*/
