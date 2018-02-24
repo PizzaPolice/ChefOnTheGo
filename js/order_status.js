a7ac59f2a1782d61365a6d504d6791b82328c92c
audreyeckman <audreyeckman@yahoo.com>
2018-02-23 16:17:52 -0800

    trying to figure out react added src

--- js/order_status.js
+++ js/order_status.js
@@ -1,3 +1,17 @@
+import React from 'react'
+import styles from './styles/main.css'
+
+
+const Hello = React.createClass({
+        render(){
+        return <div>Hello, {this.props.name}<div>
+
+React.render(<Hello name="Audrey"/>, document.body)
+
+
+
+
+
 function countdown()
 {
         var now = new Date();
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './app.css'
+
+const App = ()  => (
+	<h1>Hello World</h1>
+	)
+
+export default App
