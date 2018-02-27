914b92c4da25b431f35b627e8a7a8aabb8260b5b
Kevin <k5mao@ucsd.edu>
2018-02-26 19:41:14 -0800

    Review working

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -1,31 +1,49 @@
-class review exends React-Component{
+import React, { Component } from 'react';
+
+class Review extends Component{
 
   render()
   {
     return(
+      <div>
       <h1>Thank you for using Chef on the Go!</h1>
       <h2>Please let us know about your experience Below</h2>
 
       <div className="rating">
       <form>
       <h4>My Experience was ...</h4>
-      <input type="radio" name="star" value="1">Absolutely Terrible
-      <input type="radio" name="star" value="2">Sub-par
-      <input type="radio" name="star" value="3">Good
-      <input type="radio" name="star" value="4">Very Good
-      <input type="radio" name="star" value="5">Amazing!
+      <label>
+      Absolutely Terrible
+      <input type="radio" name="star" value="1"></input>
+      </label>
+      <label>
+      Sub-par
+      <input type="radio" name="star" value="2"></input>
+      </label>
+      <label>
+      Good
+      <input type="radio" name="star" value="3"></input>
+      </label>
+      <label>
+      Very Good
+      <input type="radio" name="star" value="4"></input>
+      </label>
+      <label>
+      Amazing
+      <input type="radio" name="star" value="5"></input>
+      </label>
       </form>
 
       <textarea name="How was your experience?" rows="10" cols="70">Enter your review here! Be sure to be descriptive, "It was good" tells fellow connoisseurs almost nothing about the chef.</textarea>
 
       <div>
       <form onSubmit = {this.handleSubmit}>
-      <input type="submit" className="review" value="Submit">  
-      <input type="submit" className="noreview" value="I don't want to leave a review">
+      <input type="submit" className="review" value="Submit"></input>
+      <input type="submit" className="noreview" value="I don't want to leave a review"></input>
       </form>
       </div>
       </div>
-
+      </div>
 
     );
   }
@@ -36,4 +54,4 @@ class review exends React-Component{
 
 }
 
-export default review
+export default Review
