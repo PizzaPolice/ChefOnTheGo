399c77b44d77168762b25637f5eb3421139d0b31
bluej-21 <jae916@gmail.com>
2018-02-28 14:23:01 -0800

    test reconfigure

--- my-app/src/__tests__/Login.test.js
+++ my-app/src/__tests__/Login.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import Login from './components/login.js'
+import App from '../App';
+import Login from '../components/login.js'
 
 
 it('logs you in', () =>{
