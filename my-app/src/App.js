07ccd8d483d93ba4563058a463e198b129db73dd
Kevin <k5mao@ucsd.edu>
2018-02-28 14:17:43 -0800

    Added routes for review and chat

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Login from './components/login.js';
-
+import Chat from './components/chat.jsx'
+import Review from './components/review.jsx'
 class App extends Component {
   render() {
     return (
@@ -20,6 +21,8 @@ class App extends Component {
           <hr/>
           <Route exact path="/" component={Login} />
           <Route path="/login" component={Login} />
+          <Route path="review" component={Review}/>
+          <Route path="chat" component={Chat}/>
         </div>
       </Router>
     );
