f56c65e0f26c594b9c6cb301a63ca3c4af211523
bluej-21 <jae916@gmail.com>
2018-03-01 00:39:16 -0800

    Working on refactoring to react

---
+++ my-app/src/components/list_chefs.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class ListChefs extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chefs: []
+        };
+
+        this.retriveChefs();
+    }
+
+    retriveChefs() {
+        var localStorage = window.localStorage;
+        var listOfChefs = JSON.parse(localStorage.getItem("chef"));
+        this.setState({chefs: listOfChefs})
+    }
+
+    ChefList(props) {
+        const chefs = props.list;
+        const listItems = chefs.map((chef) =>
+            <li>{chef}</li>
+        );
+        return (<ul>{listItems}</ul>);
+    }
+
+    render() {
+        return (
+        <div className="outer-panel">
+            <div className="panel-body">
+                <h2 className="title">List of Chefs</h2>
+                <div className="input-fields">
+                    <ChefList list={this.state.chefs} />
+                </div>
+            </div>
+            <div className="panel-footer">
+                <Link to="login">
+                    <button className="btn back-btn">Go Back</button>
+                </Link>
+                <Link to="chat">
+                    <button className="btn next-btn">Chat!</button>
+                </Link>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default ListChefs;
