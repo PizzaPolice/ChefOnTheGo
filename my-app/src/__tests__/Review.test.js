c70ca694473d8fdac8146b4da06f864c3a51f30e
audreyeckman <audreyeckman@yahoo.com>
2018-03-01 09:54:49 -0800

    add review test

---
+++ my-app/src/__tests__/Review.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount, render } from 'enzyme';
+import Review from '../components/review.jsx'
+
+it('Review component renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Review />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+// This test is shallow rendering our Review component
+describe('Review Component', () => {
+    it('Review should render without throwing an error', () => {
+        expect(shallow(<Review />).exists(<form className='Review'></form>)).toBe(true)
+      })
+    });
