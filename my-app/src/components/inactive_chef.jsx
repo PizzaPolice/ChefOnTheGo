b424b3d1f0f163920cc7eefbdb2a0ff710cb8e9d
Kevin <k5mao@ucsd.edu>
2018-03-01 16:00:19 -0800

    inactive_chef partially implmeented, state access still breaks horribly

--- my-app/src/components/inactive_chef.jsx
+++ my-app/src/components/inactive_chef.jsx
@@ -2,6 +2,16 @@ import React,{Component} from 'react';
 
 class InactiveChef extends Component
 {
+  constructor(props)
+  {
+    super(props);
+    this.state=
+    {
+      minutes: "",
+      hours: ""
+    }
+  }
+
   render()
   {
     return(
@@ -10,11 +20,11 @@ class InactiveChef extends Component
   <title>Inactive Chef</title>
 
   <h2>You are currently inactive</h2>
-  <form className="input" action="active_chef.html" onSubmit={this.validateTime}>
+  <form className="input" onSubmit={this.validateTime}>
     <h3>How long do you want to be active until</h3>
-    <input type="number" name="hour" defaultValue="0"></input>
+    <input type="number" name="hour" onChange={this.updateHours.bind(this)}defaultValue="0"></input>
     <b>Hours</b>
-    <input type="number" name="minute" defaultValue ="0"></input>
+    <input type="number" name="minute" onChange={this.updateMinute.bind(this)} defaultValue ="0"></input>
     <b>Minutes</b>
     <input type="submit" value="Start Serving!"></input>
   </form>
@@ -24,9 +34,28 @@ class InactiveChef extends Component
       
     );
   }
+  updateMinute(e)
+  {
+    this.setState({
+      minutes:e.target.value
+    });
+    alert("MINUTES");
+    
+  }
 
+  updateHours(e)
+  {
+    this.setState({
+      minutes:e.target.value
+    });
+  }
   validateTime()
   {
+    alert("VALIDATINGSDFS");
+    if (this.state.minutes < 0 || this.state.hours < 0 )
+    {
+      alert("BAD VALUES");
+    }
     alert("VALIDATING TIME");
   }
 }
