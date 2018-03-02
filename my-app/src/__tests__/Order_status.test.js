f1075bcbd9f5f0caeea31dbc1872a9518eab9dd2
audreyeckman <audreyeckman@yahoo.com>
2018-03-02 14:45:53 -0800

    checkout test

--- my-app/src/__tests__/Order_status.test.js
+++ my-app/src/__tests__/Order_status.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, mount, render } from 'enzyme';
-import Checkout from '../components/checkout.jsx'
+import OrderStatus from '../components/order_status.jsx'
 
 
 describe('Login Component', () => {
     it('Login should render without throwing an error', () => {
-        expect(shallow(<Checkout />).exists(<form className='Login'></form>)).toBe(true)
+        expect(shallow(<OrderStatus />).exists(<form className='Login'></form>)).toBe(true)
       })
       it("should render initial layout", () => {
-        const component = shallow(<Checkout />);
+        const component = shallow(<OrderStatus />);
         expect(component.getElements()).toMatchSnapshot();
         })
     });
@@ -18,20 +18,13 @@ describe('Login Component', () => {
 
     describe('Submit Button', () => {
         it('should be defined', () => {
-          expect(Checkout).toBeDefined();
+          expect(OrderStatus).toBeDefined();
         });
 
         it('should render correctly', () => {
           const button = shallow(
-            <Checkout name='btn next-btn' />
+            <OrderStatus name='btn next-btn' />
           );
           expect(button).toMatchSnapshot();
         });
-
-        it('should render correctly', () => {
-          const button = shallow(
-            <Checkout name='btn back-btn' />
-          );
-          expect(button).toMatchSnapshot();
-       });
     });
