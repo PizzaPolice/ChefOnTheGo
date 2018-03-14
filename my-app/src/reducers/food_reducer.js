b62897f89a28d215dafdcec3016bbe926a4a2e85
bluej-21 <jae916@gmail.com>
2018-03-14 14:44:44 -0700

    fixed reducers

--- my-app/src/reducers/food_reducer.js
+++ my-app/src/reducers/food_reducer.js
@@ -1,23 +1,17 @@
 import { ADD_FOOD, DELETE_FOOD } from '../actions/food_actions.js'
 
-export default function food_reducer(state=[], action) {
-
-  var copyFoodList = state.splice();
+export default function food_reducer(state = [], action) {
   switch (action.type)
   {
     case ADD_FOOD:
-      copyFoodList.push(action.payload);
-      return copyFoodList;
+        return [ ...state, action.payload];
+    
     case DELETE_FOOD:
-      for (var x = 0; x < copyFoodList.length; x ++)
-      {
-        if (copyFoodList[x]["dish_name"] === action.payload["dish_name"] && copyFoodList[x]["chef_name"] === action.payload["chef_name"])
-        {
-          copyFoodList.splice(x,1);
-        }
-      }
-      return copyFoodList;
+        return [...state].filter(food => {
+            return food["name"] !== action.payload.name;
+        });
+        
     default:
-      return state
+      return state;
   }
 }
