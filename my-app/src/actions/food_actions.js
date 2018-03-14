d333dadbfa8bf8ab7c1446c296efb9f2b2c777bf
Kevin <k5mao@ucsd.edu>
2018-03-14 13:33:25 -0700

    Added delete_dish action

--- my-app/src/actions/food_actions.js
+++ my-app/src/actions/food_actions.js
@@ -2,7 +2,16 @@
 export const ADD_FOOD = "ADD_FOOD"
 export const DELETE_FOOD = "DELETE_FOOD"
 
+
+//Action creators
 export function addFood(food)
 {
   return {type:ADD_FOOD,food}
 }
+
+export function deleteFood(food)
+{
+  return { type: DELETE_FOOD, food }
+}
+
+
