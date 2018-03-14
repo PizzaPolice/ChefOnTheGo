f254c8d67f885e142e7c6e2cf26ca1dc44fc20ca
Kevin <k5mao@ucsd.edu>
2018-03-14 14:22:54 -0700

    Initial implementation

--- my-app/src/reducers/food_reducer.js
+++ my-app/src/reducers/food_reducer.js
@@ -1,14 +1,22 @@
 import { ADD_FOOD, DELETE_FOOD } from '../actions/food_actions.js'
 
 export default function food_reducer(state=[], action) {
+
+  var copyFoodList = state.splice();
   switch (action.type)
   {
     case ADD_FOOD:
-      return [
-      
-      ]
+      copyFoodList.push(action.payload);
+      return copyFoodList;
     case DELETE_FOOD:
-      return []
+      for (var x = 0; x < copyFoodList.length; x ++)
+      {
+        if (copyFoodList[x]["dish_name"] === action.payload["dish_name"] && copyFoodList[x]["chef_name"] === action.payload["chef_name"])
+        {
+          copyFoodList.splice(x,1);
+        }
+      }
+      return copyFoodList;
     default:
       return state
   }
